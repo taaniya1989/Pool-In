c0a4babb7f91c703a615a0bdb523bc2d3adaab6e
tanvi <10vidhate@gmail.com>
2018-04-29 10:59:03 -0700

    navigation menu gui

--- app/src/main/java/edu/sdsu/tvidhate/pool_in/activity/MainActivity.java
+++ app/src/main/java/edu/sdsu/tvidhate/pool_in/activity/MainActivity.java
@@ -82,17 +82,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_add_trip) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_my_trips) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_my_profile) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_requests) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sign_out) {
 
         }
 
